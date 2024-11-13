c3d3953feb67cf8bd71c00cdc8308ddc047f9999
morshedsajid26 <neazmorshed407@gmail.com>
2024-11-13 20:59:07 +0600

    .

--- src/Components/Page/Cart.jsx
+++ src/Components/Page/Cart.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Container from '../Layer/Container'
+import Button from '../Layer/Button'
 import Bredcumb from '../Layer/Bredcumb'
 import { ImCross } from 'react-icons/im'
 import { Image } from 'antd'
@@ -36,7 +37,34 @@ const Cart = () => {
                 </div>
 
                 <div>1</div>
-                <div>1</div>
+                <div><h6 className='font-DM text-[20px] font-bold text-[#262626]'>$44.00</h6></div>
+              </div>
+
+
+              <div className='flex justify-between border border-t-0 p-5'>
+                <input className=' py-2.5 pl-2.5' type="text" placeholder='Apply coupon' />
+                <button className='font-DM text-[14px] font-bold text-[#262626]'>Update cart</button>
+              </div>
+
+
+              <div className='mt-14 flex flex-col items-end'>
+                <h5 className='font-DM text-[20px] font-bold text-[#262626]'>Cart totals</h5>
+
+                <table className='my-7'>
+                  <tr>
+                    <td className='border py-4 pl-5 pr-[234px]'><h6 className='font-DM text-[14px] font-bold text-[#262626]'>Subtotal</h6></td>
+                    <td className='border py-4 pl-5 pr-[234px]'><h6 className='font-DM text-[16px] text-[#767676]' >389.99 $</h6></td>
+                  </tr>
+
+                  <tr>
+                  <td className='border py-4 pl-5 pr-[234px]'><h6 className='font-DM text-[14px] font-bold text-[#262626]'>Total</h6></td>
+                  <td className='border py-4 pl-5 pr-[234px]'><h6 className='font-DM text-[16px] text-[#262626]' >389.99 $</h6></td>
+                  </tr>
+                </table>
+
+                <Button className='w-[200px]' ButtonText='Proceed to Checkout'/>
+                  
+                
               </div>
             </div>
         </Container>
